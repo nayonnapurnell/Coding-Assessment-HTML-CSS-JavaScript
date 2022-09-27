ca78e7b3942df707882989db4d6b1543a346618a
“Nayonna <Nayonna Purnell>
2022-09-26 22:32:10 -0400

    Added the starter files

---
+++ script.js
@@ -0,0 +1,47 @@
+// this is the starting point for this commentary,
+// please add more steps
+// there may be more steps and corrections to be made in future
+console.log("Sam, your questions");
+// need a collection of objects that hold the questions
+// need to keep a count of the question that is displayed
+// need to keep score
+// need to keep the time
+
+
+function startTimer(){
+
+}
+function addQuestion(){
+  // create the question elements
+
+  // modify the text/attributes
+  // modify one of the attributes to say right or wrong
+
+  // append all elements to something on the page
+
+  // for all the buttons, add event listener
+
+    // WHEN I answer a question
+    // if answered correctly
+    // add points
+    // show as correct
+    // WHEN I answer a question incorrectly
+    // THEN time is subtracted from the clock
+    // show as incorrect
+
+    // after the answer, 
+    // THEN I am presented with another question
+    // need a function call to present the question
+
+    // if no other questions,
+    // go to highscore screen
+}
+
+function startQuiz(){
+  // timer starts and I am presented with a question
+  // need a function call to start the timer
+
+  // need a function call to present the question
+}
+
+// eventListener here // when i click the start button
