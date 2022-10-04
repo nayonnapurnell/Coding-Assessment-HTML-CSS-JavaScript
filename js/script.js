a0487c7ffe08e99ffc6d6550cfa8c11d22e4373e
“Nayonna <Nayonna Purnell>
2022-10-03 21:05:58 -0400

    Added the ul for the answer choices

--- js/script.js
+++ js/script.js
@@ -1,87 +1,119 @@
-//Quiz Elements
-var startButton = document.getElementById('start-quiz-button');
-var quizContent = document.querySelectorAll('.quiz-questions');
-var quizQuestions = document.createElement('div');
+
+
+var quizQuestionsArray = [
+  {
+    id: 1,
+    question: "Commonly used data-types DO NOT include:",
+      a: "Strings",
+      b: "Booleans",
+      c: "alerts",
+      d: "numbers",
+    answer: "c: alerts"
+  },
+  {
+    id: 2,
+    question: "The condition in an if/else statement is enclosed within ____.",
+    a: "quotes",
+    b: "curly brackets",
+    c: "parentheses",
+    d: "square brackets",
+  answer: "c: parentheses"
+  },
+  {
+    id: 3,
+    question: "Arrays in JavaScript can be used to store?",
+    a: "numbers and strings",
+    b: "other arrays",
+    c: "booleans",
+    d: "all of the above",
+  answer: "d: all of the above"
+  },
+  {
+    id: 4,
+    question: "String values must be enclosed within _______ when being assigned to variables.",
+    a: "commas",
+    b: "curly brackets",
+    c: "quotes",
+    d: "parentheses",
+  answer: "c: quotes"
+  }
+];
+
+
 
 //Timer Elements
-var startingMinutes = 1;
+var startingMinutes = 5;
 var time = startingMinutes * 60;
 var countDownTimer = document.querySelector('#time-Counter');
 
+//Quiz Elements
+var startButton = document.getElementById('start-quiz-button');
+var quizContent = document.querySelectorAll('.quiz-content');
+var quizQuestions = document.querySelectorAll('.quiz-questions');
+
+
+console.log(quizQuestions);
+console.log("I am here");
+
+
 //Start Button Event Listener
 startButton.addEventListener("click", function(event){
-  
 //Quiz Countdown Timer - 5 minutes
 var interval = setInterval(startTimer, 1000);
-function startTimer() {
-  var minutes = Math.floor(time/60);
-  var seconds = time % 60;
+    function startTimer() {
+      var minutes = Math.floor(time/60);
+      var seconds = time % 60;
 
-  seconds = seconds < 10 ? '0' + seconds : seconds;
-  countDownTimer.innerHTML = `Time: ${minutes}:${seconds}`;
-  time-- 
-  if (time < 0){
-    clearInterval(interval);
-  } 
-}
+      seconds = seconds < 10 ? '0' + seconds : seconds;
+      countDownTimer.innerHTML = `Time: ${minutes}:${seconds}`;
+      time-- 
+      if (time < 0){
+        clearInterval(interval);
+      } 
+    }; 
+    
+    
+    addQuestion();
+
 })
+//End of the Start Button code
+
+
+
+  
+ 
+
+
+  
 
 //Hides quiz questions from the page
-for(i = 0; i <quizContent.length; i++){
-  quizContent[i].style.display = 'block';
-}
+// for(i = 0; i <quizContent.length; i++){
+//   quizContent[i].style.display = 'block';
+// }
+
+
+
+
+
+
+
+
 
 //Create the quiz questions
 function addQuestion(){
-  var score = 0;
 
-  var quizQuestions = [
-    {
-      id: 1,
-      question: "Commonly used data-types DO NOT include:",
-      answerChoice: {
-        a: "Strings",
-        b: "Booleans",
-        c: "alerts",
-        d: "numbers",
-      },
-      answer: "c: alerts"
-    },
-    {
-      id: 2,
-      question: "The condition in an if/else statement is enclosed within ____.",
-      answerChoice: {
-      a: "quotes",
-      b: "curly brackets",
-      c: "parentheses",
-      d: "square brackets"
-    },
-    answer: "c: parentheses"
-    },
-    {
-      id: 3,
-      question: "Arrays in JavaScript can be used to store?",
-      answerChoice: {
-      a: "numbers and strings",
-      b: "other arrays",
-      c: "booleans",
-      d: "all of the above"
-    },
-    answer: "d: all of the above"
-    },
-    {
-      id: 4,
-      question: "String values must be enclosed within _______ when being assigned to variables.",
-      answerChoice: {
-      a: "commas",
-      b: "curly brackets",
-      c: "quotes",
-      d: "parentheses"
-    },
-    answer: "c: quotes"
-    }
-  ]
+
+  var nums = [15, 15, 14, 14, 13, 12, 12, 11, 11, 11, 28, 28];
+  
+  for (i = 0; i < arr.length; ++i) {
+    arr[i].textContent = nums[i];
+  }
+
+  //var score = 0;
+
+ 
   // create the question elements
+  //var quizQuestions = document.querySelectorAll('.quiz-questions');
 
   // modify the text/attributes
   // modify one of the attributes to say right or wrong
@@ -113,15 +145,27 @@ function startQuiz(){
 
   // need a function call to present the question
 
-  //set display to either none or block*****
-
-  
+  //set display to either none or block*****  
 }
 
-// eventListener here // when i click the start button
 
-var answer = [];
-//querySelector gets you exactly one tag
+/*{ <div class="score"></div>
+<div class="score"></div>
+<div class="score"></div>
+<div class="score"></div>
+<div class="score"></div>
+<div class="score"></div>
+<div class="score"></div>
+<div class="score"></div>
+<div class="score"></div>
+<div class="score"></div>
+<div class="score"></div>
+<div class="score"></div>
+<div class="score"></div>
+<div class="score"></div>
+<div class="score"></div> }*/
+
+
 
 
 
